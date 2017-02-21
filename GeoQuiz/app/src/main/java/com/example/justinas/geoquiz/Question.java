289608ce470c17fefd07f063d611c7d688c09359
Justinasjocius <justinas@Justinass-MacBook-Pro.local>
2017-02-21 14:06:15 +0100

    1st Chapter of book finished

---
+++ GeoQuiz/app/src/main/java/com/example/justinas/geoquiz/Question.java
@@ -0,0 +1,8 @@
+package com.example.justinas.geoquiz;
+
+/**
+ * Created by justinas on 2/21/17.
+ */
+
+public class Question {
+}
---
+++ Tingle/app/src/main/java/com/example/justinas/tingle/Thing.java
@@ -0,0 +1,17 @@
+package com.example.justinas.tingle;
+
+/**
+ * Created by justinas on 2/8/17.
+ */
+
+public class Thing {
+    private String mWhat= null; private String mWhere= null;
+    public Thing(String what, String where) { mWhat= what;
+        mWhere= where; }
+    @Override
+    public String toString() { return oneLine("is here: "); }
+    public String getWhat() { return mWhat; }
+    public void setWhat(String what) { mWhat= what; } public String getWhere() { return mWhere; }
+    public void setWhere(String where) { mWhere= where; } public String oneLine(String post) {
+        return mWhat + " "+post + mWhere; }
+}
