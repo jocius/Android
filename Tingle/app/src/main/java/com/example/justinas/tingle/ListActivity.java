e1c50a8fef25d733079370438a2741e86daaa2ed
Justinasjocius <justinas@Justinass-MacBook-Pro.local>
2017-03-01 17:18:00 +0100

    Mandatory Tingle finished

--- Tingle/app/src/main/java/com/example/justinas/tingle/ListActivity.java
+++ Tingle/app/src/main/java/com/example/justinas/tingle/ListActivity.java
@@ -1,5 +1,6 @@
 package com.example.justinas.tingle;
 
+import android.content.res.Configuration;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
@@ -16,14 +17,22 @@ public class ListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            FragmentManager fm = getSupportFragmentManager();
+            Fragment fragment = fm.findFragmentById(R.id.activityListFragmentContainer);
+            if (fragment == null) {
+                fragment = new TingleListFragment();
+                fm.beginTransaction()
+                        .add(R.id.activityListFragmentContainer, fragment)
+                        .commit(); }
 
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.activityListFragmentContainer);
-        if (fragment == null) {
-            fragment = new TingleListFragment();
-            fm.beginTransaction()
-                    .add(R.id.activityListFragmentContainer, fragment)
-                    .commit(); }
+        }
+        else{
+
+            finish();
+        }
+
+
         System.out.println("Orientation - portrait");
 
     }
