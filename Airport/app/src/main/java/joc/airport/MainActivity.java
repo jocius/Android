75ac98082f56a99f33559e8f80f1e799638dbb69
Justinasjocius <justinas@Justinass-MacBook-Pro.local>
2017-04-19 17:43:11 +0200

    airport- beacon works as well as update of UI

--- Airport/app/src/main/java/joc/airport/MainActivity.java
+++ Airport/app/src/main/java/joc/airport/MainActivity.java
@@ -1,16 +1,40 @@
 package joc.airport;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.estimote.coresdk.common.requirements.SystemRequirementsChecker;
+import com.estimote.coresdk.recognition.packets.Beacon;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private Button start;
+    private TextView mInput;
+    private List<Beacon> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start = (Button) findViewById(R.id.button1);
+        mInput = (TextView) findViewById(R.id.hello);
+
+
+
+        final MyApplication app  = new MyApplication();
+        start.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+             list = app.startIt();
+                updateUI(list.get(0).toString());
+            }
+        });
+
     }
 
 
@@ -20,4 +44,11 @@ public class MainActivity extends AppCompatActivity {
 
         SystemRequirementsChecker.checkWithDefaultDialogs(this);
     }
+
+    public void updateUI(String input){
+
+        mInput.setText(input);
+    }
+
+
 }
--- Airport/app/src/main/java/joc/airport/MyApplication.java
+++ Airport/app/src/main/java/joc/airport/MyApplication.java
@@ -5,43 +5,117 @@ package joc.airport;
  */
 
 import android.app.Application;
-
+import android.util.Log;
 
 
 import com.estimote.coresdk.observation.region.Region;
 import com.estimote.coresdk.observation.region.beacon.BeaconRegion;
+import com.estimote.coresdk.recognition.packets.Beacon;
 import com.estimote.coresdk.service.BeaconManager;
 import com.estimote.coresdk.observation.region.AbstractRegionMonitor;
 import com.estimote.mgmtsdk.connection.protocol.Operation;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 
 public class MyApplication extends Application {
 
     private BeaconManager beaconManager;
-    private Region region;
+    private BeaconRegion region;
+    private static List<Beacon> newList1 = new ArrayList<Beacon>();
+
+
+
 
     @Override
     public void onCreate() {
-        super.onCreate();
 
+        super.onCreate();
+        System.out.println("On created@@@@!!");
         beaconManager = new BeaconManager(getApplicationContext());
+
+        beaconManager.disconnect();
+//// We want the beacons heartbeat to be set at one second.
+        beaconManager.setBackgroundScanPeriod(TimeUnit.SECONDS.toMillis(5),
+                5);
+        beaconManager.setForegroundScanPeriod(TimeUnit.SECONDS.toMillis(5),
+                5);
+
+
+        beaconManager.setRangingListener(new BeaconManager.BeaconRangingListener() {
+            @Override
+            public void onBeaconsDiscovered(BeaconRegion beaconRegion, List<Beacon> list) {
+
+
+                System.out.println("entered...");
+                if (list.size()>0){
+                    newList1.add(list.get(0));
+                    System.out.println("List size: " + list.size());
+                System.out.println("MAJOR: " + list.get(0).getMajor());
+                System.out.println("MAJOR: " + list.get(0).getMinor());
+                    System.out.println("RSS value: " + list.get(0).getRssi());
+                    System.out.println("measured power " + list.get(0).getMeasuredPower());
+
+
+
+                }
+            }
+        });
+//        beaconManager.setMonitoringListener(new BeaconManager.BeaconMonitoringListener() {
+//            @Override
+//            public void onEnteredRegion(BeaconRegion beaconRegion, List<Beacon> list) {
+//                System.out.println("entered...");
+//                if (list.size()>0){
+//                System.out.println("MAJOR: " + list.get(0).getMajor());
+//                System.out.println("MAJOR: " + list.get(0).getMinor());
+//                    Log.d("Airport", "Nearest places: " + list.get(0).getMinor());
+//                }
+//            }
+//
+//            @Override
+//            public void onExitedRegion(BeaconRegion beaconRegion) {
+//                System.out.println("exited");
+//            }
+//        });
+
+        region = new BeaconRegion("ranged region", UUID.fromString("e3b54450-ab73-4d79-85d6-519eaf0f45d9"), null, null);
 
         beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
             @Override
             public void onServiceReady() {
 
-
-                beaconManager.startMonitoring(new BeaconRegion("monitored region",
-                        UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), null, null));
-
-
+                beaconManager.startRanging(region);
             }
         });
 
+
+//        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+//            @Override
+//            public void onServiceReady() {
+//
+//
+//                beaconManager.startMonitoring(new BeaconRegion("monitored region",
+//                        UUID.fromString("e3b54450-ab73-4d79-85d6-519eaf0f45d9"), null, null));
+//
+//
+//            }
+//        });
+
+
+
+
 }
 
-\
+
+    public  List<Beacon> startIt(){
+
+                return newList1;
+
+    }
+
+
 }
